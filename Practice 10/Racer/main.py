c6d0683df268d3c6ee8d9df133b63b51fcbe15dc
Mura <vv3rcett1vv@gmail.com>
2026-04-21 17:12:41 +0400

    Practice 10, RACER

--- Practice 10/Racer/main.py
+++ Practice 10/Racer/main.py
@@ -2,88 +2,104 @@ import pygame, sys, time
 from pygame.locals import *
 import random
  
+#Initalize  pygame
 pygame.init()
- 
+
+#Frame per second controller
 FPS = 60
 FramePerSec = pygame.time.Clock()
 
+#Basic colors
 BLUE  = (0, 0, 255)
 RED   = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
+#Resolution of the window
 SCREEN_WIDTH = 400
 SCREEN_HEIGHT = 700
 
- 
+#Create a white screen
 DISPLAYSURF = pygame.display.set_mode((400,700))
 DISPLAYSURF.fill(WHITE)
 pygame.display.set_caption("Game")
  
- 
+
+#Setting up Sprites, Enemy
 class Enemy(pygame.sprite.Sprite):
-      def __init__(self):
+    #Insert the image of Enemy, and set its location
+    def __init__(self):
         super().__init__() 
-        self.image = pygame.image.load("Racer/images/Enemy.png")
+        self.image = pygame.image.load("Practice 10/Racer/images/Enemy.png")
         self.rect = self.image.get_rect()
         self.rect.center=(random.randint(40,SCREEN_WIDTH-40),0) 
- 
-      def move(self):
+
+    #Logic for the movement of Enemy
+    def move(self):
+        #Speed of the Enemy
         self.rect.move_ip(0,6)
         if (self.rect.bottom > 700):
             self.rect.top = 0
+            #Three strips where the Enemy can appear, and it will appear randomly in one of the three strips
             self.rect.center = random.choice([(70, 0), (200, 0), (330, 0)])
  
- 
+
+#Setting up Sprites, Player
 class Player(pygame.sprite.Sprite):
+    #Insert the image of Player, and set its location
     def __init__(self):
         super().__init__() 
-        self.image = pygame.image.load("Racer/images/Player.png")
+        self.image = pygame.image.load("Practice 10/Racer/images/Player.png")
         self.rect = self.image.get_rect()
         self.rect.center = (160, 600)
- 
+    
+    #Logic for the movement of Player
     def move(self):
+        #Get the set of keys pressed and check for user input
         pressed_keys = pygame.key.get_pressed()
+
         if self.rect.left > 0:
               if pressed_keys[K_LEFT]:
-                  self.rect.move_ip(-7, 0)
+                    #Move left if the left arrow key is pressed
+                    self.rect.move_ip(-7, 0)
         if self.rect.right < SCREEN_WIDTH:        
               if pressed_keys[K_RIGHT]:
-                  self.rect.move_ip(7, 0)    
+                    #Move right if the right arrow key is pressed
+                    self.rect.move_ip(7, 0)    
 
+
+#Setting up Sprites, Coin
 class Coin(pygame.sprite.Sprite):
+    #Insert the image of Coin, and set its location
     def __init__(self):
         super().__init__() 
-        self.image = pygame.image.load("Racer/images/Coin.png")
+        self.image = pygame.image.load("Practice 10/Racer/images/Coin.png")
         self.rect = self.image.get_rect()
         self.rect.center= (160, 660) 
  
+    #Logic for the movement of Coin
     def move(self):
         self.rect.move_ip(0,6)
+        #Randomly appear in one of the three strips when it goes out of the screen
         if (self.rect.bottom > 700):
             self.rect.top = 0
             self.rect.center = random.choice([(70, 0), (200, 0), (330, 0)]) 
  
 
+#Setting up Sprites, Enemy, Player and Coin
 P1 = Player()
 E1 = Enemy()
 C1 = Coin()
 
-enemies = pygame.sprite.Group()
-enemies.add(E1)
-coins = pygame.sprite.Group()
-coins.add(C1)
-all_sprites = pygame.sprite.Group()
-all_sprites.add(P1)
-all_sprites.add(E1)
-all_sprites.add(C1)
-
+#Custom event for increasing speed of Enemy and Coin
 INC_SPEED = pygame.USEREVENT + 1
 pygame.time.set_timer(INC_SPEED, 1000)
 
 
+
 def game_loop():
+    #Creating Sprites Groups
     enemies = pygame.sprite.Group()
     enemies.add(E1)
     coins = pygame.sprite.Group()
@@ -92,9 +108,13 @@ def game_loop():
     all_sprites.add(P1)
     all_sprites.add(E1)
     all_sprites.add(C1)
-    SUM = 0
+
+    SUM = -1
+
+    #Game Loop
     while True:
         SPEED = 5 
+        #Event handling, gets all event from the event queue
         for event in pygame.event.get():
             if event.type == INC_SPEED:
                 SPEED += 2
@@ -106,25 +126,33 @@ def game_loop():
     
         DISPLAYSURF.fill(WHITE)
 
+
+        #Check if the Player has collected the Coin, and count it
         if pygame.sprite.spritecollideany(P1, coins):
             SUM += 1
             C1.rect.top = 0
             C1.rect.center = random.choice([(70, 0), (200, 0), (330, 0)])
 
+            #update to show the new score after collecting the coin
+            pygame.display.update()
+
+        #If enemy intersects with the coin, then dont show the coin
         if pygame.sprite.spritecollideany(E1, coins):
-            SUM += 1
             C1.rect.top = 0
             C1.rect.center = random.choice([(70, 0), (200, 0), (330, 0)])
 
+        #Display the score of coins collected
         font = pygame.font.SysFont("Elephent", 32)
         text = font.render("Coins Collected: " + str(SUM), True, BLACK)
-        DISPLAYSURF.blit(text, (50, 50))
-        pygame.display.update()
-    
+        DISPLAYSURF.blit(text, (30, 30))
+
+
+        #Move each sprite
         for entity in all_sprites:
             DISPLAYSURF.blit(entity.image, entity.rect)
             entity.move()
-    
+
+        #Check if any of the enemies have collided with the Player
         if pygame.sprite.spritecollideany(P1, enemies):
             waiting = True
             while waiting:
@@ -160,13 +188,13 @@ def game_loop():
             
                 
 
-
+        #Update the display and set the frame rate
         pygame.display.update()
         FramePerSec.tick(FPS)
 
 
 
-
+#Start the game loop
 restart = True
 while restart:
     restart = game_loop()
